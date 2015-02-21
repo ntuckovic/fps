35d932f91f89ef962aabb04d2f3d8e20f085a332
ntuckovic <ntuckovic@gmail.com>
2015-02-21 13:40:52 +0100

    statika

---
+++ fps_glavni/static/js/glavni-graf.js
@@ -0,0 +1,19 @@
+//glavni graf
+
+var chart = c3.generate({
+	bindto: '#graph-container',
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    }
+});
