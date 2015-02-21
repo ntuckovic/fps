f769651149745e188839e057f5cae71821292069
Ines Jelovac <ines.jelovac@gmail.com>
2015-02-21 13:36:08 +0100

    slug za stranke, obrisana šifra za prihode

--- fps_glavni/models.py
+++ fps_glavni/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class PoliticalParty(models.Model):
     name = models.CharField(verbose_name=u"Naziv stranke", max_length=200)
     short_name = models.CharField(verbose_name=u"Kratki naziv", max_length=50)
+    slug = models.SlugField(verbose_name=u"slug", max_length=200, unique=True, db_index=True)
 
     def __unicode__(self):
         return self.short_name
@@ -12,8 +13,16 @@ class PoliticalParty(models.Model):
         verbose_name = u"Politička stranka"
         verbose_name_plural = u"Političke stranke"
 
+# INCOME_TYPES = (
+#     (1, u'Prihodi iz državnog proračuna'),
+#     (2, u'Prihodi iz proračuna jedinica lokalne i područne (regionalne) samouprave'),
+#     (3, u'Prihodi od imovine'),
+#     (4, u'Prihodi od donacija'),
+#     (5, u'Drugi prihodi'),
+# )
+
 class Income(models.Model):
-    code = models.SmallIntegerField(verbose_name=u"Šifra")
+    # code = models.SmallIntegerField(verbose_name=u"Šifra", choices=INCOME_TYPES)
     name = models.CharField(verbose_name=u"Naziv prihoda", max_length=200)
 
     def __unicode__(self):
@@ -26,11 +35,11 @@ class Income(models.Model):
 class Amount(models.Model):
     party = models.ForeignKey(PoliticalParty, verbose_name=u"Politička stranka")
     income = models.ForeignKey(Income, verbose_name=u"Prihod")
-    amount = models.DecimalField(verbose_name=u"Iznos", decimal_places=2, max_digits=20)
+    amount = models.DecimalField(verbose_name=u"Iznos", decimal_places=2, max_digits=20, default=0)
     year = models.SmallIntegerField(verbose_name=u"Godina")
 
     def __unicode__(self):
-        return self.party.short_name + ", " + self.income + ": " + str(self.amount)
+        return self.party.short_name + ", " + self.income.name + ": " + str(self.amount)
 
     class Meta:
         verbose_name = u"Iznos"
