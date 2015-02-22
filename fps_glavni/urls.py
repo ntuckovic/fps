1bf5c8218c09c459d4b5f2cfb3c07eefef333845
ntuckovic <ntuckovic@gmail.com>
2015-02-22 11:33:45 +0100

    dodani api link

--- fps_glavni/urls.py
+++ fps_glavni/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name="fps_index"),
     url(r'stranka/(?P<slug>\w+)$', PartyView.as_view(), name="fps_party"),
     url(r'impressum/$', ImpressumView.as_view(), name="fps_impressum"),
-    url(r'database-dump/$', JsonDbDump.as_view(content_type='application/json')),
+    url(r'database-dump/$', JsonDbDump.as_view(content_type='application/json'), name="database_dump"),
 )
